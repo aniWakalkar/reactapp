1935b6b09cd218464b00aa42f11af321272fa21b
aniWakalkar <aniket.wakalkar@caredose.com>
2024-06-20 15:21:15 +0530

    top padding issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import Navbar from './components/Navbar';
@@ -7,14 +7,15 @@ import Logout from "./components/auth/Logout";
 import Signup from "./components/auth/Signup";
 
 function App() {
+  const [formData, setFormData] = useState({});
   return (
     <div className='container mx-auto p-4 bg-gray-100'>
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup setFormData={setFormData}/>} />
+        <Route path="/login" element={<Login formData={formData} />} />
         <Route path="/logout" element={<Logout />} />
       </Routes>
     </Router>
